94b9e743f827e7853acaa770e7bdd7ffd493e17c
Mateusz Kusiak <mateusz.kusiak@3mdeb.com>
2025-04-08 15:03:37 +0200

    runqemu.sh: allow custom paths
    Allow custom paths passed as sysargs in runqemu.sh script. An image can
    be downloaded of builder and run locally.
    
    Signed-off-by: Mateusz Kusiak <mateusz.kusiak@3mdeb.com>

--- runqemu.sh
+++ runqemu.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
+# Default image path
+DEFAULT_IMAGE="./build/x11ssh/tmp/deploy/images/x11ssh/obmc-phosphor-image-x11ssh.static.mtd"
+
+# Use the first argument as image path if provided, else use default
+IMAGE_PATH="${1:-$DEFAULT_IMAGE}"
+
+# Check if the file exists
+if [ ! -f "$IMAGE_PATH" ]; then
+  echo "Error: File '$IMAGE_PATH' not found."
+  exit 1
+fi
+
+# Run QEMU with the verified image
 qemu-system-arm -machine supermicrox11-bmc \
-  -drive file=./build/x11ssh/tmp/deploy/images/x11ssh/obmc-phosphor-image-x11ssh.static.mtd,format=raw,if=mtd \
+  -drive file="$IMAGE_PATH",format=raw,if=mtd \
   -m 256 \
   -nographic \
   -net nic \
